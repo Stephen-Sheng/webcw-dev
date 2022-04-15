2774e0789e81357a445ed07ed4d383793de79d9e
YutongSheng <544518449@qq.com>
2022-04-15 17:49:03 +0100

    improve route and login state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { NotFoundBoundary, Router, View } from 'react-navi';
 import './App.css';
 import {routes} from './routes'
+import { userReducer } from './reducers'
+import React, { useReducer } from 'react';
+import { UserContext } from './context';
 
 function App() {
+
+  const [user, dispatch] = useReducer(userReducer, '')
+
   return (
+    <UserContext.Provider value={{ user, dispatch }}>
     <Router routes={routes}>
       <NotFoundBoundary render={() => <h1>404! Not Found</h1>}>
         <View />
       </NotFoundBoundary>
     </Router>
+    </UserContext.Provider>
+
   );
 }
 
--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,6 +1,35 @@
+import Navmenu from "../Navmenu"
+import Sidermenu from "../Sidermenu"
+import { Layout, Breadcrumb } from 'antd';
+
+const { Content } = Layout;
 
 export default function Page1(){
 
 
-    return <div>Content1</div>
+    return (
+        <Layout>
+        <Navmenu />
+        <Layout>
+          <Sidermenu />
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              Content
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    )
 }
