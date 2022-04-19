c95236a745abb32ee2a8535b1b91321ef0729754
wyb <dongyi-bin@163.com>
2022-04-19 18:36:23 +0100

    basic express

---
+++ backend/controllers/login.js
@@ -0,0 +1,22 @@
+const dbConfig = require("../util/dbconfig");
+
+//登陆接口
+log = (req, res)=>{
+    let {username,password} = req.query;
+    var sql = "SELECT * FROM cw.user WHERE name=? AND password=?";
+    var sqlArr = [username, password];
+    var callBack = (err, data)=>{
+        if(err){
+            console.log('connection failed')
+        } else {
+            if(data.length){
+                console.log('登陆成功')
+            } else {
+                console.log('用户名或密码错误')
+            }
+        }
+    }
+    dbConfig.sqlConnect(sql, sqlArr, callBack)
+}
+
+module.exports = {log};
