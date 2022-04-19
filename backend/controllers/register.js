ef3cc1cd6e61f12c0ff12985cf91ebd176e9070a
wyb <dongyi-bin@163.com>
2022-04-19 18:36:23 +0100

    basic express

---
+++ backend/controllers/register.js
@@ -0,0 +1,26 @@
+const dbConfig = require("../util/dbconfig");
+
+//注册接口
+reg = (req, res)=>{
+    let {username,password} = req.query;
+    var sql = "SELECT * FROM cw.user WHERE name=?";
+    var sqlArr = [username];
+    var callBack = async (err, data)=>{
+        if(err){
+            console.log('connection failed')
+        } else {
+            if(data.length){
+                console.log('zhuceguole')
+            } else {
+                //没有注册过，写入数据库
+                let sql_insert = "INSERT into cw.user(name,password) value(?,?)";
+                let sqlArr_insert = [username,password];
+                let res = await dbConfig.SySqlConnect(sql_insert, sqlArr_insert);
+                console.log('zhucechenggong')
+            }
+        }
+    }
+    dbConfig.sqlConnect(sql, sqlArr, callBack)
+}
+
+module.exports = {reg};
