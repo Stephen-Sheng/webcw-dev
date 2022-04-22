defcc1fc8a87bf9bff5f91f6e46d3e6516ffc3e2
wyb <dongyi-bin@163.com>
2022-04-22 20:54:53 +0100

    resInfo

---
+++ backend/controllers/resInfo.js
@@ -0,0 +1,18 @@
+const dbConfig = require("../util/dbconfig");
+
+//餐厅列表接口
+resIn = (req, res)=> {
+    let {resId} = req.query;
+    var sql = "SELECT itemId,itemName,price,description,figure FROM cw.item WHERE resId=?";
+    var sqlArr = [resId];
+    var callBack = (err, data) => {
+        if(err){
+            console.log('connection failed')
+        } else {
+            res.send(data)
+            console.log(data)
+        }
+    }
+    dbConfig.sqlConnect(sql, sqlArr, callBack)
+}
+module.exports = {resIn};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,11 @@ var user = require('../controllers/userController')
 var register = require('../controllers/register')
 var login = require('../controllers/login')
 var resList = require('../controllers/resList')
+var resInfo = require('../controllers/resInfo')
 router.get('/', user.getUser);
 router.get('/getOrder', user.getOrder)
 router.post('/login', login.log)
 router.post('/register', register.reg)
-router.get('/resList',resList.res)//路径
+router.get('/resList',resList.res)
+router.get('/resInfo',resInfo.resIn)
 module.exports = router;
