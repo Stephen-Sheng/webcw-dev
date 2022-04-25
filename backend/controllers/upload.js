d68dc9f655274a51ea8945baa915da9b90a14d74
wyb <dongyi-bin@163.com>
2022-04-25 11:11:32 +0100

    test upload

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -4,7 +4,15 @@ const dbConfig = require("../util/dbconfig");
 upload = (req, res) => {
     let {orderInfo} = req.body;
     console.log(req.body);
+    var sql = "";
+    var sqlArr = [];
+    var callBack = (err, data) => {
+        if(err){
+            console.log("upload fail")
+        } else {
 
+        }
+    }
     dbConfig.sqlConnect(sql, sqlArr, callBack)
 }
 module.exports = {upload};
