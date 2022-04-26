1dd1b5334e4a22ff68b06c586ab028caa0060cb6
wyb <dongyi-bin@163.com>
2022-04-26 11:07:49 +0100

    resInfo update

--- backend/controllers/resInfo.js
+++ backend/controllers/resInfo.js
@@ -2,6 +2,7 @@ const dbConfig = require("../util/dbconfig");
 
 //餐厅列表接口
 resIn = (req, res)=> {
+    var value = null;
     let {resId} = req.query;
     var sql = "SELECT itemId,itemName,price,description,figure FROM cw.item WHERE resId=?";
     var sqlArr = [resId];
@@ -9,7 +10,21 @@ resIn = (req, res)=> {
         if(err){
             console.log('connection failed')
         } else {
-            res.send(data)
+            value = data;
+            console.log(value);
+            var sql2 = "SELECT resName FROM cw.restaurant WHERE resId=?";
+            var sqlArr2 = [resId];
+            var callBack2 = (err, data) => {
+                if(err){
+                    console.log('failed');
+                } else {
+                    var resp = data[0];
+                    var key = "menu";
+                    resp[key] = value;
+                    res.send(resp);
+                }
+            }
+            dbConfig.sqlConnect(sql2, sqlArr2, callBack2)
         }
     }
     dbConfig.sqlConnect(sql, sqlArr, callBack)
